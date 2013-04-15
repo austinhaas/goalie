bc43ec9b89887a5fd43c8a89fb0f3bd6fd4dc8b8
Austin Haas <austin@pettomato.com>
2013-04-15 11:04:46 -0700

    Bump core.logic version. Improve defproject description.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.pettomato/goalie "0.1.0"
-  :description "This library can be used to unobtrusively instrument core.logic goals."
+  :description "A library for unobtrusively instrumenting core.logic goals."
   :url "https://github.com/austinhaas/goalie"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.1"]]
+                 [org.clojure/core.logic "0.8.3"]]
   :source-paths ["src/com"])
